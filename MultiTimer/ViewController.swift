a63fcffe1a21f3589ff0810374763ee231c5427f
Stu <gera-stu@ya.ru>
2021-07-08 11:50:13 +0300

    Initial Commit

---
+++ MultiTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiTimer
+//
+//  Created by Сэнди Белка on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
